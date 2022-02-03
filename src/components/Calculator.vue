b84881d7bb9da3fcec912203ad714ecf8ea2ae1d
Kristian Aars <kristian@aars.me>
2022-02-03 15:54:36 +0100

    Added HTML and styling for calculator

---
+++ src/components/Calculator.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Calculator</h1>
+
+  <div id="main-container">
+    <div id="calculator-container">
+      <p id="display">25</p>
+
+      <div id="button-grid">
+        <!-- First button row -->
+        <button class="calculator-button function-button">C</button>
+        <button class="calculator-button function-button">ANS</button>
+        <button class="calculator-button function-button">DEL</button>
+        <button class="calculator-button operation-button">+</button>
+
+        <!-- Second button row -->
+        <button class="calculator-button">1</button>
+        <button class="calculator-button">2</button>
+        <button class="calculator-button">3</button>
+        <button class="calculator-button operation-button">-</button>
+
+        <!-- Third button row -->
+        <button class="calculator-button">4</button>
+        <button class="calculator-button">5</button>
+        <button class="calculator-button">6</button>
+        <button class="calculator-button operation-button">x</button>
+
+        <!-- Fourth button row -->
+        <button class="calculator-button">7</button>
+        <button class="calculator-button">8</button>
+        <button class="calculator-button">9</button>
+        <button class="calculator-button operation-button">/</button>
+
+        <!-- Fifth button row -->
+        <button class="calculator-button"></button>
+        <button class="calculator-button">0</button>
+        <button class="calculator-button">,</button>
+        <button class="calculator-button operation-button">=</button>
+      </div>
+
+    </div>
+
+    <div id="log-container">
+      <h3>Calculation log</h3>
+      <textarea id="log">
+
+      </textarea>
+    </div>
+  </div>
+
+</template>
+
+<script>
+require("@/assets/styles/calculator.css");
+
+export default {
+  name: "Calculator",
+  actions: {
+
+  }
+};
+</script>
+
+<style scoped src="@/assets/styles/calculator.css" />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Calculator />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import Calculator from "@/components/Calculator.vue"
 
 export default {
-  name: "Home",
+  name: "CalculatorHome",
   components: {
-    HelloWorld,
+    Calculator,
   },
 };
 </script>
